243b827a5b2183642761d9679f81a910688291f9
dickm19 <maryclairedick@gmail.com>
2021-03-08 14:12:00 -0500

    added drop shadow to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,22 @@ export default function Home(){
 
             <Link to={"/menu"}>
                 <span className='title'>Full-stack Web Developer</span>
-            </Link>
+            </Link><br/>
+            <svg>
+                <defs>
+                    <filter id="drop-shadow">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="shadow" />
+                        <feOffset in="shadow" dx="3" dy="4" result="shadow" />
+                        <feColorMatrix in="shadow" type="matrix" 
+                            values="0 0 0 0 0  
+                            0 0 0 0 0  
+                            0 0 0 0 0  
+                            0 0 0 0.6 0" 
+                            result="shadow" />
+                        <feBlend in="SourceGraphic" in2="shadow" />
+                    </filter>
+                </defs>
+            </svg>
         </div>
     )
 }
